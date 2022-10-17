b02c47438db7811ae2ed53052cc01acef1945025
Dmitry andimeon <andimeon@gmail.com>
2022-10-17 14:12:29 +0900

    Implement async routers

--- backend/foodcart/api/endpoints/banners.py
+++ backend/foodcart/api/endpoints/banners.py
@@ -1,20 +1,28 @@
-from fastapi import APIRouter, Depends
+import aiofiles
+from fastapi import APIRouter, Depends, UploadFile, File, Form
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from backend.db.db_deps import get_db
 from backend.foodcart.schemas.banners import BannerOut, BannerIn
-from backend.foodcart.crud.crud_banners import get_banners, create_banner
+from backend.foodcart.crud.crud_banners import create_banner, get_banners
+from backend.foodcart.utils.save_file import save_images_to_media
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[BannerOut])
-def read_banners(db: Session = Depends(get_db)) -> list[BannerOut]:
-    banners = get_banners(db)
+@router.get('/', response_model=list[BannerOut])
+async def read_banners(db: Session = Depends(get_db)) -> list[BannerOut]:
+    banners = await get_banners(db)
     return banners
 
 
-@router.post('/', response_model=BannerIn, status_code=201)
-def create_new_banner(banner_in: BannerIn, db: Session = Depends(get_db)) -> BannerOut:
-    banner = create_banner(db, banner_in)
-    return banner
+@router.post('/', status_code=201)
+async def create_new_banner(
+    banner_picture: UploadFile,
+    banner_in: BannerIn = Depends(),
+    db: Session = Depends(get_db),
+) -> BannerOut:
+    await save_images_to_media(banner_picture)
+    banner = await create_banner(db, banner_in, banner_picture.filename)
+    return {'filename': banner_picture.filename, 'banner': banner}
