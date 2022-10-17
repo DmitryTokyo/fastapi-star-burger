dd69f5ba74ef7ac5ba8e3286687e0e501138faf9
Dmitry andimeon <andimeon@gmail.com>
2022-10-17 14:10:57 +0900

    Update settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,10 +5,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = '/api/v1'
     SECRET_KEY: str = os.environ.get('SECRET_KEY', default=secrets.token_urlsafe(32))
-    DB_URL: str = 'postgresql://andimeon:565651db@localhost:5432/star_burger_fast'
+    DB_URL: str = 'postgresql+asyncpg://andimeon:565651db@localhost:5432/star_burger_fast'
     PROJECT_NAME: str = 'Fast api star burger version'
+
+    MEDIA_ROOT: str = './backend/media/'
 
     class Config:
         case_sensitive = True
