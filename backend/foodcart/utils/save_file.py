9581881cd0e96112f8761eb234b6d42911aae0ab
Dmitry andimeon <andimeon@gmail.com>
2022-10-17 14:18:15 +0900

    Implement save images to media folder

---
+++ backend/foodcart/utils/save_file.py
@@ -0,0 +1,11 @@
+import aiofiles
+from fastapi import UploadFile
+
+from backend.config.settings import settings
+
+
+async def save_images_to_media(file: UploadFile) -> None:
+    file_path = f"{settings.MEDIA_ROOT}img/{file.filename}"
+    async with aiofiles.open(file_path, "wb") as buffer:
+        image_content = await file.read()
+        await buffer.write(image_content)
