27db236faa44c682415ba6f3d31f74dc3ef17cc0
Dmitry andimeon <andimeon@gmail.com>
2022-10-20 12:38:24 +0900

    Update router

--- backend/foodcart/api/routers.py
+++ backend/foodcart/api/routers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from backend.foodcart.api.endpoints import banners
 from backend.foodcart.api.endpoints import restaurants
+from backend.foodcart.api.endpoints import products
 
 api_router = APIRouter()
 
 api_router.include_router(banners.router, prefix='/banners', tags=['banners'])
 api_router.include_router(restaurants.router, prefix='/restaurants', tags=['restaurants'])
+api_router.include_router(products.router, prefix='/products', tags=['products'])
