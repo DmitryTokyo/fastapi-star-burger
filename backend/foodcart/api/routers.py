d9bd51d9ef8a5115e16f6113a5f2b770b860f95e
Dmitry andimeon <andimeon@gmail.com>
2022-10-20 12:15:39 +0900

    Add restaurants endpoints to router. Add prefix to routers

--- backend/foodcart/api/routers.py
+++ backend/foodcart/api/routers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from backend.foodcart.api.endpoints import banners
+from backend.foodcart.api.endpoints import restaurants
 
 api_router = APIRouter()
-api_router.include_router(banners.router, tags=['banners'])
+
+api_router.include_router(banners.router, prefix='/banners', tags=['banners'])
+api_router.include_router(restaurants.router, prefix='/restaurants', tags=['restaurants'])
