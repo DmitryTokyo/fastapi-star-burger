0fdbdf4f50a62438da4a735f1cbcf30b49bf5609
Dmitry andimeon <andimeon@gmail.com>
2022-10-25 13:50:52 +0900

    Fix saving image on product endpoint

--- backend/foodcart/api/endpoints/products.py
+++ backend/foodcart/api/endpoints/products.py
@@ -5,7 +5,7 @@ from backend.db.db_deps import get_db
 from backend.foodcart.crud.crud_products import get_products, create_product, get_product_categories, \
     create_product_category
 from backend.foodcart.schemas.products import ProductOut, ProductIn, ProductCategoryOut, ProductCategoryIn
-from backend.foodcart.utils.save_file import save_images_to_media
+from backend.foodcart.services.images import save_image
 
 router = APIRouter()
 
@@ -21,7 +21,7 @@ async def create_new_banner(
     product_in: ProductIn = Depends(),
     db: AsyncSession = Depends(get_db),
 ) -> dict[str, ProductOut | str]:
-    await save_images_to_media(product_picture)
+    await save_image(product_picture)
     product = await create_product(db, product_in, product_picture.filename)
     return {'filename': product_picture.filename, 'banner': product}
 
