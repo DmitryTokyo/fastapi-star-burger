6c7a19302a6adc4c24a9ce2de71611ddd16e95a7
Dmitry andimeon <andimeon@gmail.com>
2022-10-25 13:43:57 +0900

    Rename save image function

--- backend/foodcart/services/images.py
+++ backend/foodcart/services/images.py
@@ -4,7 +4,7 @@ from fastapi import UploadFile
 from backend.config import settings
 
 
-async def save_images(file: UploadFile) -> None:
+async def save_image(file: UploadFile) -> None:
     file_path = f'{settings.MEDIA_ROOT}img/{file.filename}'
     async with aiofiles.open(file_path, 'wb') as buffer:
         image_content = await file.read()
