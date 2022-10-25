30f9c93105d277ab659c6c56e1db6580c9e518ed
Dmitry andimeon <andimeon@gmail.com>
2022-10-25 12:05:05 +0900

    Add backend and root path variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,21 +1,27 @@
 import os
 import secrets
+from pathlib import Path
 
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyHttpUrl
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'Fast api star burger version'
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    API_V1_STR: str = '/api/v1'
+    BACKEND_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ROOT_DIR = os.path.dirname(BACKEND_DIR)
+    API_V1_STR: str = '/api'
     SECRET_KEY: str = os.environ.get('SECRET_KEY', default=secrets.token_urlsafe(32))
 
     DB_URL: str = 'postgresql+asyncpg://andimeon:565651db@localhost:5432/star_burger_fast'
     PROJECT_NAME: str = 'Fast api star burger version'
 
-    STATIC_ROOT = StaticFiles(directory=os.path.join(BASE_DIR, "static"))
-    MEDIA_ROOT: str = './backend/media/'
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = [
+        'http://localhost',
+        'http://localhost:8000',
+    ]
+
+    MEDIA_ROOT: str = f'{BACKEND_DIR}/media/'
 
     class Config:
         case_sensitive = True
