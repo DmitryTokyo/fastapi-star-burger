8adf2c4c3f12dea76d97429cc5c4be3706a516bc
Dmitry andimeon <andimeon@gmail.com>
2022-10-25 13:41:48 +0900

    Create service images

--- backend/foodcart/services/images.py
+++ backend/foodcart/services/images.py
@@ -1,10 +1,10 @@
 import aiofiles
 from fastapi import UploadFile
 
-from backend.config.settings import settings
+from backend.config import settings
 
 
-async def save_images_to_media(file: UploadFile) -> None:
+async def save_images(file: UploadFile) -> None:
     file_path = f'{settings.MEDIA_ROOT}img/{file.filename}'
     async with aiofiles.open(file_path, 'wb') as buffer:
         image_content = await file.read()
