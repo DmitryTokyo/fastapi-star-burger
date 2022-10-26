eef5ed8ae329c4da71af16d22e67053350c37195
Dmitry andimeon <andimeon@gmail.com>
2022-10-26 13:30:08 +0900

    Add image router and endpoint

--- backend/foodcart/api/endpoints/images.py
+++ backend/foodcart/api/endpoints/images.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from pathlib import Path
+
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse
+from starlette.status import HTTP_404_NOT_FOUND
+
+from backend.config.settings import settings
 
 router = APIRouter()
 
 
-@router.get('/image', responses={200: {'content': {'image/png': {}}}})
-async def get_image(filename: str):
-    pass
+@router.get('/{filename}', responses={200: {'content': {'image/png': {}}}})
+async def get_image(filename: str) -> FileResponse | None:
+    file_path = f'{settings.MEDIA_ROOT}img/{filename}'
+    image_file = Path(file_path)
+    if not image_file.is_file():
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail='File not found')
+    return FileResponse(file_path)
