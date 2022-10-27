6a5eee334ec1ccd0132a339596f645aa99b9a5ba
Dmitry andimeon <andimeon@gmail.com>
2022-10-27 15:22:00 +0900

    Fix response model in restaurants endpoints. Implement delete method

--- backend/foodcart/api/endpoints/restaurants.py
+++ backend/foodcart/api/endpoints/restaurants.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter, Depends
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_200_OK
 
 from backend.db.db_deps import get_db
-from backend.foodcart.crud.crud_restaurants import get_restaurants, create_restaurant
+from backend.foodcart.crud.crud_restaurants import get_restaurants, create_restaurant, delete_restaurant
 from backend.foodcart.schemas.restaurants import RestaurantOut, RestaurantIn
 
 router = APIRouter()
 
 
-@router.get('/', response_model=list[RestaurantOut])
-async def read_restaurants(db: AsyncSession = Depends(get_db)) -> list[RestaurantOut]:
+@router.get('/', response_model=list[RestaurantOut], status_code=HTTP_200_OK)
+async def read_restaurants(db: AsyncSession = Depends(get_db)):
     return await get_restaurants(db)
 
 
-@router.post('/', response_model=RestaurantIn, status_code=201)
-async def create_new_restaurant(restaurant_in: RestaurantIn, db: AsyncSession = Depends(get_db)) -> RestaurantOut:
+@router.post('/', response_model=RestaurantOut, status_code=HTTP_201_CREATED)
+async def create_new_restaurant(restaurant_in: RestaurantIn, db: AsyncSession = Depends(get_db)):
     return await create_restaurant(db, restaurant_in)
+
+
+@router.delete('/', status_code=HTTP_204_NO_CONTENT)
+async def delete_exist_restaurant(restaurant_id: int, db: AsyncSession = Depends(get_db)) -> None:
+    await delete_restaurant(db, restaurant_id)
--- backend/foodcart/crud/crud_restaurants.py
+++ backend/foodcart/crud/crud_restaurants.py
@@ -1,12 +1,12 @@
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.foodcart.models.restaurants import Restaurant
-from backend.foodcart.schemas.restaurants import RestaurantIn
+from backend.foodcart.schemas.restaurants import RestaurantIn, RestaurantOut
 
 
-async def get_restaurants(db: AsyncSession) -> list[Restaurant]:
+async def get_restaurants(db: AsyncSession) -> list[RestaurantOut]:
     statement = select(Restaurant)
     db_execute = await db.execute(statement)
     return db_execute.scalars().all()
@@ -19,3 +19,9 @@ async def create_restaurant(db: AsyncSession, restaurant_in: RestaurantIn) -> Re
     await db.commit()
     await db.refresh(restaurant_obj)
     return restaurant_obj
+
+
+async def delete_restaurant(db: AsyncSession, restaurant_id: int) -> int:
+    stmt = delete(Restaurant).where(Restaurant.id == restaurant_id)
+    await db.execute(stmt)
+    await db.commit()
