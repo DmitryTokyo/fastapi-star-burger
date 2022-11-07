bad28cd85ffdcd6a82784dd9232394c06b681314
Dmitry andimeon <andimeon@gmail.com>
2022-11-07 13:50:27 +0900

    Fix image url attribute in json data

--- backend/foodcart/crud/crud_products.py
+++ backend/foodcart/crud/crud_products.py
@@ -1,20 +1,21 @@
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from backend.foodcart.models.products import Product, ProductCategory
 from backend.foodcart.schemas.products import ProductIn, ProductCategoryIn
 
 
 async def get_products(db: AsyncSession) -> list[Product]:
-    statement = select(Product)
-    db_execute = await db.execute(statement)
+    stmt = select(Product).options(selectinload(Product.product_category))
+    db_execute = await db.execute(stmt)
     return db_execute.scalars().all()
 
 
 async def create_product(db: AsyncSession, restaurant_in: ProductIn, picture_filename: str) -> Product:
     product_in_data = jsonable_encoder(restaurant_in)
-    product_in_data |= {'image_file': picture_filename}
+    product_in_data |= {'image_url': picture_filename}
     product_obj = Product(**product_in_data)
     db.add(product_obj)
     await db.commit()
