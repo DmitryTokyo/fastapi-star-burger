0000a848a58c2c1bac32e6cb1c3b623c807df172
Dmitry andimeon <andimeon@gmail.com>
2022-11-09 22:28:21 +0900

    Add ignore comment

--- backend/admin/admin.py
+++ backend/admin/admin.py
@@ -3,5 +3,5 @@ from sqladmin import ModelView
 from backend.foodcart.models.banners import Banner
 
 
-class BannerAdmin(ModelView, model=Banner):
+class BannerAdmin(ModelView, model=Banner):  # type: ignore
     column_list = [Banner.id, Banner.title]
