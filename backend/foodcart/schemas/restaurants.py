db7cf867ddcb139b6347b9854925e8a2a22db002
Dmitry andimeon <andimeon@gmail.com>
2022-11-14 15:16:55 +0900

    Add restaurant menu items schema

--- backend/foodcart/schemas/restaurants.py
+++ backend/foodcart/schemas/restaurants.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from backend.foodcart.schemas.products import ProductOut
+
 
 class RestaurantBase(BaseModel):
     title: str
@@ -15,6 +17,25 @@ class RestaurantOut(RestaurantBase):
     id: int
     longitude: float | None = None
     latitude: float | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class RestaurantMenuItemsBase(BaseModel):
+    pass
+
+
+class RestaurantMenuItemsIn(RestaurantMenuItemsBase):
+    product_id: int
+    restaurant_id: int
+
+
+class RestaurantMenuItemsOut(RestaurantMenuItemsBase):
+    id: int
+    product: ProductOut
+    restaurant: RestaurantOut
+    available: bool
 
     class Config:
         orm_mode = True
