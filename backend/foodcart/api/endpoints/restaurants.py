93c7e17d8207a025bfc36aaf55a6109df63c8f2c
Dmitry andimeon <andimeon@gmail.com>
2022-11-14 15:17:28 +0900

    Implement get and create restaurant menu items endpoints

--- backend/foodcart/api/endpoints/restaurants.py
+++ backend/foodcart/api/endpoints/restaurants.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_200_OK
 
 from backend.db.db_deps import get_db
-from backend.foodcart.crud.crud_restaurants import get_restaurants, create_restaurant, delete_restaurant
-from backend.foodcart.schemas.restaurants import RestaurantOut, RestaurantIn
+from backend.foodcart.crud.crud_restaurants import (
+    get_restaurants, create_restaurant, delete_restaurant, get_restaurant_items, create_restaurant_menu_item,
+)
+from backend.foodcart.schemas.restaurants import (
+    RestaurantOut, RestaurantIn, RestaurantMenuItemsOut, RestaurantMenuItemsIn,
+)
 
 router = APIRouter()
 
@@ -22,3 +26,16 @@ async def create_new_restaurant(restaurant_in: RestaurantIn, db: AsyncSession = 
 @router.delete('/', status_code=HTTP_204_NO_CONTENT)
 async def delete_exist_restaurant(restaurant_id: int, db: AsyncSession = Depends(get_db)) -> None:
     await delete_restaurant(db, restaurant_id)
+
+
+@router.get('/restaurant-menu-items', response_model=list[RestaurantMenuItemsOut], status_code=HTTP_200_OK)
+async def read_restaurant_menu_items(db: AsyncSession = Depends(get_db)):
+    return await get_restaurant_items(db)
+
+
+@router.post('/restaurant-menu-items', response_model=RestaurantMenuItemsOut, status_code=HTTP_201_CREATED)
+async def create_new_restaurant_menu_item(
+        restaurant_menu_item_in: RestaurantMenuItemsIn,
+        db: AsyncSession = Depends(get_db),
+):
+    return await create_restaurant_menu_item(db, restaurant_menu_item_in)
--- backend/foodcart/crud/crud_restaurants.py
+++ backend/foodcart/crud/crud_restaurants.py
@@ -1,9 +1,13 @@
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
-from backend.foodcart.models.restaurants import Restaurant
-from backend.foodcart.schemas.restaurants import RestaurantIn, RestaurantOut
+from backend.foodcart.models.products import Product
+from backend.foodcart.models.restaurants import Restaurant, RestaurantMenuItem
+from backend.foodcart.schemas.restaurants import (
+    RestaurantIn, RestaurantOut, RestaurantMenuItemsOut, RestaurantMenuItemsIn,
+)
 
 
 async def get_restaurants(db: AsyncSession) -> list[RestaurantOut]:
@@ -23,5 +27,32 @@ async def create_restaurant(db: AsyncSession, restaurant_in: RestaurantIn) -> Re
 
 async def delete_restaurant(db: AsyncSession, restaurant_id: int) -> None:
     stmt = delete(Restaurant).where(Restaurant.id == restaurant_id)
+    await db.execute(stmt)
+    await db.commit()
+
+
+async def get_restaurant_items(db: AsyncSession) -> list[RestaurantMenuItemsOut]:
+    stmt = select(RestaurantMenuItem).options(
+        selectinload(RestaurantMenuItem.product).subqueryload(Product.product_category),
+        selectinload(RestaurantMenuItem.restaurant),
+    )
+    db_execute = await db.execute(stmt)
+    return db_execute.scalars().all()
+
+
+async def create_restaurant_menu_item(
+        db: AsyncSession,
+        restaurant_menu_item_in: RestaurantMenuItemsIn,
+) -> RestaurantMenuItem:
+    restaurant_menu_item_in_data = jsonable_encoder(restaurant_menu_item_in)
+    restaurant_menu_item_obj = RestaurantMenuItem(**restaurant_menu_item_in_data)
+    db.add(restaurant_menu_item_obj)
+    await db.commit()
+    await db.refresh(restaurant_menu_item_obj)
+    return restaurant_menu_item_obj
+
+
+async def delete_restaurant_menu_item(db: AsyncSession, restaurant_menu_item_id: int) -> None:
+    stmt = delete(RestaurantMenuItem).where(RestaurantMenuItem.id == restaurant_menu_item_id)
     await db.execute(stmt)
     await db.commit()
