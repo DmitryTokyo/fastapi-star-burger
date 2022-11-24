9dc9fcd2d86248f9d8154a2e7cdcd46e15ba69ed
Dmitry andimeon <andimeon@gmail.com>
2022-11-24 14:08:29 +0900

    Remove verbose name from model

--- backend/foodcart/models/banners.py
+++ backend/foodcart/models/banners.py
@@ -11,7 +11,7 @@ class Banner(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(200), index=True)
     description = Column(Text, nullable=True)
-    banner_order = Column('banner position', Integer, default=0, index=True)
+    banner_order = Column(Integer, default=0, index=True)
     image_file = Column(Text, nullable=False)
 
     def __str__(self) -> str:
