895d438dda2e7416ba64e3e0e54b9f056d621314
Dmitry andimeon <andimeon@gmail.com>
2022-12-01 13:26:34 +0900

    Add update banner updating test

--- backend/foodcart/tests/test_cruds.py
+++ backend/foodcart/tests/test_cruds.py
@@ -1,9 +1,9 @@
 import pytest
 from sqlalchemy import insert, select
 
-from backend.foodcart.crud.crud_banners import get_banners, create_banner
+from backend.foodcart.crud.crud_banners import get_banners, create_banner, delete_banner, update_banner
 from backend.foodcart.models.banners import Banner
-from backend.foodcart.schemas.banners import BannerOut, BannerIn
+from backend.foodcart.schemas.banners import BannerOut, BannerIn, BannerUpdate
 
 
 async def test_get_banners(insert_object_to_db, test_session):
@@ -17,7 +17,7 @@ async def test_get_banners(insert_object_to_db, test_session):
         image_file='test.jpg',
         id=1,
     )
-    await insert_object_to_db(stmt)
+    await insert_object_to_db(stmt, Banner)
 
     banners = await get_banners(test_session)
     banner_out = BannerOut.from_orm(banners[0])
@@ -39,3 +39,27 @@ async def test_create_banner(test_session, get_all_object_from_db, banner_in, pi
 
     assert banner == expected_objects[0]
     assert banner.id == expected_objects[0].id
+
+
+async def test_delete_banner(insert_object_to_db, test_session, get_all_object_from_db):
+    stmt = insert(Banner).values(
+        title='test_title', description='test description', banner_order=1, image_file='test.jpg',
+    )
+    await insert_object_to_db(stmt, Banner)
+
+    await delete_banner(test_session, 1)
+    expected_objects = await get_all_object_from_db(select(Banner).where(Banner.id == 1))
+
+    assert len(expected_objects) == 0
+
+
+async def test_update_banner(insert_object_to_db, test_session, get_single_object_from_db):
+    stmt = insert(Banner).values(
+        title='test_title', description='test description', banner_order=1, image_file='test.jpg',
+    )
+    initial_obj = await insert_object_to_db(stmt, Banner)
+    banner_update = BannerUpdate(title='updated_title')
+
+    updated_banner_obj = await update_banner(test_session, initial_obj.id, banner_update)
+
+    assert updated_banner_obj.title == banner_update.title
