0ae35533cb437551a2a1aa3f4815356d8bab7813
Dmitry andimeon <andimeon@gmail.com>
2022-12-06 13:36:01 +0900

    Transform banner crud methods to new implementation based on base crud module

--- backend/foodcart/api/endpoints/banners.py
+++ backend/foodcart/api/endpoints/banners.py
@@ -1,18 +1,19 @@
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, HTTPException
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_200_OK
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK
 
 from backend.db.db_deps import get_db
 from backend.foodcart.schemas.banners import BannerOut, BannerIn, BannerUpdate
-from backend.foodcart.crud.crud_banners import create_banner, get_banners, delete_banner, update_banner
 from backend.star_burger.utils.images import save_image_to_server
+from backend.foodcart.crud.banner import crud_banner
 
 router = APIRouter()
 
 
 @router.get('/', response_model=list[BannerOut], status_code=HTTP_200_OK)
 async def read_banners(db: AsyncSession = Depends(get_db)):
-    return await get_banners(db)
+    return await crud_banner.get_multi(db)
 
 
 @router.post('/', status_code=HTTP_201_CREATED)
@@ -22,13 +23,18 @@ async def create_new_banner(
     db: AsyncSession = Depends(get_db),
 ) -> dict[str, BannerOut | str]:
     await save_image_to_server(banner_picture)
-    banner = await create_banner(db, banner_in, banner_picture.filename)
+    banner = await crud_banner.create(db, banner_in, image_file=banner_picture.filename)
     return {'filename': banner_picture.filename, 'banner': banner}
 
 
-@router.delete('/', status_code=HTTP_204_NO_CONTENT)
-async def delete_exist_banner(banner_id: int, db: AsyncSession = Depends(get_db)) -> None:
-    await delete_banner(db, banner_id)
+@router.delete('/', status_code=HTTP_200_OK)
+async def delete_exist_banner(banner_id: int, db: AsyncSession = Depends(get_db)) -> dict[str, int]:
+    try:
+        await crud_banner.get_single(db, banner_id)
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail='Object does not found')
+    deleted_banner_id = await crud_banner.delete(db, banner_id)
+    return {'deleted banner id': deleted_banner_id}
 
 
 @router.patch('/{banner_id}/image', response_model=BannerOut, status_code=HTTP_200_OK)
@@ -37,9 +43,14 @@ async def update_exist_banner_image(
     banner_image: UploadFile,
     db: AsyncSession = Depends(get_db),
 ):
+    try:
+        await crud_banner.get_single(db, banner_id)
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail='Object does not found')
+
     await save_image_to_server(banner_image)
     banner_update = BannerUpdate(image_file=banner_image.filename)
-    return await update_banner(db, banner_id, banner_update)
+    return await crud_banner.update(db, banner_update, banner_id)
 
 
 @router.patch('/{banner_id}', response_model=BannerOut, status_code=HTTP_200_OK)
@@ -48,4 +59,9 @@ async def update_exist_banner(
     banner_update: BannerUpdate,
     db: AsyncSession = Depends(get_db),
 ):
-    return await update_banner(db, banner_id, banner_update)
+    try:
+        await crud_banner.get_single(db, banner_id)
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail='Object does not found')
+
+    return await crud_banner(db, banner_update, banner_id)
---
+++ backend/foodcart/crud/banner.py
@@ -0,0 +1,10 @@
+from backend.foodcart.crud.base import CRUDBase
+from backend.foodcart.models.banners import Banner
+from backend.foodcart.schemas.banners import BannerIn, BannerOut, BannerUpdate
+
+
+class CRUDBanner(CRUDBase[Banner, BannerIn, BannerUpdate]):
+    pass
+
+
+crud_banner = CRUDBanner(Banner)
