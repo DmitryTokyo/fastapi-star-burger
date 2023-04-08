47c01f3329c6c7aaaa13ea18c654c6893ea33c40
andimeon <andimeon@gmail.com>
2023-04-08 20:01:43 +0900

    Fix flake8 errors: white spaces

--- backend/star_burger/utils/images.py
+++ backend/star_burger/utils/images.py
@@ -6,11 +6,10 @@ from backend.config.settings import settings
 
 
 async def save_image_to_server(file: UploadFile) -> None:
-    file_dir = Path(settings.MEDIA_ROOT) / "img"
+    file_dir = Path(settings.MEDIA_ROOT) / 'img'
     file_dir.mkdir(parents=True, exist_ok=True)
     file_path = file_dir / file.filename
-    
+
     async with aiofiles.open(file_path, 'wb') as buffer:
         image_content = await file.read()
         await buffer.write(image_content)
-
