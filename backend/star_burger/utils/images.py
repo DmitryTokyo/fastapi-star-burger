0c08399c9c674cb46f9dbaee66ac52625d60ec5c
andimeon <andimeon@gmail.com>
2023-04-08 11:51:13 +0900

    Create media directory if it is not exist

--- backend/star_burger/utils/images.py
+++ backend/star_burger/utils/images.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import aiofiles
 from fastapi import UploadFile
 
@@ -5,7 +6,11 @@ from backend.config.settings import settings
 
 
 async def save_image_to_server(file: UploadFile) -> None:
-    file_path = f'{settings.MEDIA_ROOT}img/{file.filename}'
+    file_dir = Path(settings.MEDIA_ROOT) / "img"
+    file_dir.mkdir(parents=True, exist_ok=True)
+    file_path = file_dir / file.filename
+    
     async with aiofiles.open(file_path, 'wb') as buffer:
         image_content = await file.read()
         await buffer.write(image_content)
+
